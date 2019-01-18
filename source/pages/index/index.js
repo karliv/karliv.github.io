0d24813c7433e464fde8983f9bdfcedcb8fa6af9
karliv <kyzmikboy@mail.ru>
2019-01-18 13:36:35 +0300

    initial commit

---
+++ source/pages/index/index.js
@@ -0,0 +1,6 @@
+import './css/index.sass';
+import './css/fonts.sass';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'bootstrap/dist/js/bootstrap.min';
+
